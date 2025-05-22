7e247a4e75953766ab3580a11789e0bd9a6dad36
scnene23 <scnene23@gmail.com>
2025-05-22 15:52:39 +0100

    termos atualização

--- src/components/protectionPolicy/page.jsx
+++ src/components/protectionPolicy/page.jsx
@@ -41,24 +41,7 @@ const ProtectionPolicyForm = ({
                                     </ModalHeader>
                                     <ModalBody className="flex flex-col mx-5 my-5 space-y-8 max-h-96 overflow-y-auto bg-background text-textPrimaryColor">
                                         <p>
-                                            A Quinta da Vacaria 1616 – Vinhos, S.A (adiante designada “QV”), com sede sita na Quinta da Vacaria, 5050-364 Vilarinho dos Freires, Peso da Régua, Portugal e NIPC 50035988, é a responsável pelo tratamento.
-                                            Os dados pessoais recolhidos serão utilizados unicamente para registo na newsletter da QV após obtermos o seu consentimento prévio e expresso, e para que possamos enviar-lhe outras publicações que nos pediu e/ou que possam ser do seu interesse, nomeadamente, ofertas especiais ou informar sobre atualizações do website.
-                                            O seu “email” é de preenchimento obrigatório para podermos enviar a newsletter, pelo que sem este dado não poderemos realizar o seu pedido. Por sua vez, o seu “nome”, permitem-nos personalizar comunicação.
-                                            Os dados em questão não serão utilizados para nenhuma outra finalidade sem o seu consentimento prévio, sem ser informado previamente e quando necessário recolhido o seu consentimento, sendo conservados pela QV durante 2 anos.
-                                            Relembramos que poderá retirar o presente consentimento a qualquer momento.
-                                            Quando nos envia os seus dados pessoais, estes serão protegidos através de protocolos de segurança adequados às comunicações via internet e serão conservados num servidor seguro e de acesso condicionado.
-                                            A QV poderá comunicar os dados pessoais identificados ao(s) seu(s) prestador(es) de serviços para cumprimento da finalidade descrita.
-                                            Poderá exercer os seus direitos previstos na legislação (acesso, retificação, apagamento, limitação, oposição, portabilidade) através de contacto escrito para o seguinte e-mail: geral@quintadavacaria.pt, sem prejuízo do direito a apresentar reclamação à autoridade de controlo competente (www.cnpd.pt).
-                                            Saiba mais sobre a forma como os seus dados serão tratados Política de Privacidade ou através do e-mail: geral@quintadavacaria.pt
-                                            A Quinta da Vacaria 1616 – Vinhos, S.A (adiante designada “QV”), com sede sita na Quinta da Vacaria, 5050-364 Vilarinho dos Freires, Peso da Régua, Portugal e NIPC 50035988, é a responsável pelo tratamento.
-                                            Os dados pessoais recolhidos serão utilizados unicamente para registo na newsletter da QV após obtermos o seu consentimento prévio e expresso, e para que possamos enviar-lhe outras publicações que nos pediu e/ou que possam ser do seu interesse, nomeadamente, ofertas especiais ou informar sobre atualizações do website.
-                                            O seu “email” é de preenchimento obrigatório para podermos enviar a newsletter, pelo que sem este dado não poderemos realizar o seu pedido. Por sua vez, o seu “nome”, permitem-nos personalizar comunicação.
-                                            Os dados em questão não serão utilizados para nenhuma outra finalidade sem o seu consentimento prévio, sem ser informado previamente e quando necessário recolhido o seu consentimento, sendo conservados pela QV durante 2 anos.
-                                            Relembramos que poderá retirar o presente consentimento a qualquer momento.
-                                            Quando nos envia os seus dados pessoais, estes serão protegidos através de protocolos de segurança adequados às comunicações via internet e serão conservados num servidor seguro e de acesso condicionado.
-                                            A QV poderá comunicar os dados pessoais identificados ao(s) seu(s) prestador(es) de serviços para cumprimento da finalidade descrita.
-                                            Poderá exercer os seus direitos previstos na legislação (acesso, retificação, apagamento, limitação, oposição, portabilidade) através de contacto escrito para o seguinte e-mail: geral@quintadavacaria.pt, sem prejuízo do direito a apresentar reclamação à autoridade de controlo competente (www.cnpd.pt).
-                                            Saiba mais sobre a forma como os seus dados serão tratados Política de Privacidade ou através do e-mail: geral@quintadavacaria.pt
+                                            
                                         </p>
                                     </ModalBody>
                                 </form>
